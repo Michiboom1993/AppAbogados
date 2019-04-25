917372681af13437d93911f2bf2c7f93a033382d
markitos <markomas9@gmail.com>
2019-04-25 22:50:45 +0200

    ea

--- app/src/main/java/com/example/marki/appabogados/Chat/HolderMensaje.java
+++ app/src/main/java/com/example/marki/appabogados/Chat/HolderMensaje.java
@@ -12,7 +12,7 @@ public class HolderMensaje extends RecyclerView.ViewHolder {
     private TextView nombre;
     private TextView mensaje;
     private TextView hora;
-    private CircleImageView fotoMensajePerfil;
+    private CircleImageView fotoVerga;
     private ImageView fotoMensaje;
 
     public HolderMensaje(View itemView) {
@@ -20,7 +20,7 @@ public class HolderMensaje extends RecyclerView.ViewHolder {
         nombre = (TextView) itemView.findViewById(R.id.nombreMensaje);
         mensaje = (TextView) itemView.findViewById(R.id.mensajeMensaje);
         hora = (TextView) itemView.findViewById(R.id.horaMensaje);
-        fotoMensajePerfil = (CircleImageView) itemView.findViewById(R.id.fotoPerfilMensaje);
+        fotoVerga = (CircleImageView) itemView.findViewById(R.id.fotoPerfilMensaje);
         fotoMensaje = (ImageView) itemView.findViewById(R.id.mensajeFoto);
     }
 
